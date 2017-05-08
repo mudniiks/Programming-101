e10ba144b357ed1db9ccd109ccc3fd56437231bd
mudniiks <mudniiks@gmail.com>
2017-05-08 20:49:34 +0200

    no message

---
+++ New Unity Project/Assets/CameraFollowPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour {
+
+	Transform _player;
+
+	Transform player {
+		get{
+			if (_player == null)
+				_player = FindObjectOfType<PlayerMovement> ().transform;
+		return _player;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3 (player.position.x, transform.position.y, player.position.z);
+	}
+}
---
+++ New Unity Project/Assets/PlayerMovement.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour {
+
+	[System.Serializable]
+	public class Controls{
+		public string horizontalAxis;
+		[SerializeField]
+		public string verticalAxis;
+		[SerializeField]
+		public KeyCode aimButton;
+
+	}
+
+	Rigidbody _playerRigidbody;
+	Rigidbody playerRigidbody {
+		get {
+			if (_playerRigidbody == null)
+				_playerRigidbody = GetComponent<Rigidbody> ();
+			return _playerRigidbody;
+		}
+	}
+
+	public Controls playerControls;
+
+	public float _speed;
+	public float speed {
+		get {
+			if (Input.GetKey (playerControls.aimButton))
+				return _speed / 2;
+			else
+				return _speed;
+		}
+	}
+		
+	// Update is called once per frame
+	void Update () {
+		Vector2 direction = new Vector2(Input.GetAxis(playerControls.horizontalAxis), Input.GetAxis(playerControls.verticalAxis));
+		Move (direction);
+		//makes player aim at mouse position
+		Aim();
+	}
+	void Move(Vector2 movement) {
+		playerRigidbody.velocity = new Vector3 (movement.x * speed, playerRigidbody.velocity.y, movement.y * speed);
+		}
+	void Aim() {
+		//position of the mouse on the screen (in screen coordinates)
+		Vector3 mouseScreenPosition = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.y);
+		Debug.Log (mouseScreenPosition);
+		// position of the mouse in the game world
+		Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint (mouseScreenPosition);
+		// make the player look at the mouse game position
+		transform.LookAt(new Vector3(mouseWorldPosition.x, transform.position.y, mouseWorldPosition.z));
+	}
+}
+
